0ce16b7f40a1bb68e92a3a48af37946fba5d527f
kachu16 <kajolsharma286@gmail.com>
2025-12-16 13:48:42 +0530

    login-api-created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import db from '../config/db.js';
 import bcrypt from 'bcryptjs';
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -13,7 +14,7 @@ router.post("/register", (req, res) => {
         [email],
         (err, result) => {
             if (err) {
-                return res.send({ messsage: "Error while adding user", error: err })
+                return res.send({ messsage: "Error while checking mail", error: err })
             }
 
             console.log(result)
@@ -34,6 +35,50 @@ router.post("/register", (req, res) => {
             )
         }
     )
+})
+
+
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    //checkEmailQuery
+    const checkEmailQuery = "Select * from user_data where email = ?";
+    db.query(checkEmailQuery, [email], (err, result) => {
+        if (err) {
+            return res.send({ message: "Error in checking mail id", error: err })
+        }
+        if (result.length === 0) {
+            return res.send({ message: "User doesn't exist" })
+        }
+
+        const user = result[0];
+        //check password
+        const isPasswordCorrect = bcrypt.compareSync(password, user.PASSWORD);
+        if (!isPasswordCorrect) {
+            return res.send({ message: "Password doesn't match" })
+        }
+
+        //token generate
+        const token = jwt.sign(
+            {
+                id: user.id,
+                email: user.email
+            }, process.env.JWT_SECRET,
+            { expiresIn: '1d' }
+        )
+
+        //store cookie
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "strict",
+            maxAge: 24 * 60 * 60 * 1000
+        })
+        return res.status(200).json({
+            message: "Login successful"
+        });
+
+    })
 })
 
 export default router;
